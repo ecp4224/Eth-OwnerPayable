94eafb50398fdbd72596ece870dfb7129fff4f52
hypereddie <ecp4224@gmail.com>
2018-02-27 14:06:15 -0500

    Update contract, change license, and provide example

---
+++ Example.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.20;
+
+//TODO Come up with good example of owner pays model
+contract ExampleContract is OwnerPays {
+
+    mapping(address => uint) idk;
+
+    function myPublicFunction() public returns (uint) {
+       costlyFunction();
+       return idk[msg.sender];
+    }
+
+    function costlyFunction() private ownerPays {
+        idk[msg.sender]++;
+
+    }
+}
--- OwnerPayable.sol
+++ OwnerPayable.sol
@@ -1,52 +1,76 @@
-pragma solidity ^0.4.0;
-import 'https://github.com/zeppelin-solidity/contracts/ownership/Ownable.sol'; 
+pragma solidity ^0.4.20;
+
+/**
+ * @title OwnerPays
+ * @dev The OwnerPays contract is a type of Ownable, and thus has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ *
+ * This contract adds the ability to supply a contract with "Passive Gas" from an owner address. Passive Gas is then used
+ * to refund the caller the gas amount for any function that has the ownerPays modifier.
+ *
+ * To mark a function as owner pays, just give the function the ownerPays modifier.
+ */
 contract OwnerPays is Ownable {
-    private uint passiveGasSpendingLimit = 5000;
-    private uint totalPassiveGasSpent = 0;
-    private bool paused = false;
-    
+    uint passiveGasSpendingLimit = 5000;
+    uint totalPassiveGasSpent = 0;
+    bool paused = false;
+
     event PassiveGasSpent(uint gasAmount, uint totalSpent, uint spendingLimit);
-    
+
+    /**
+     * @dev Record the exact amount of gas spent during this function call
+     * and refund the caller the amount spent
+     */
     modifier ownerPays {
         require(!paused);
-        require(totalSpent < spendingLimit);
-        
+        require(totalPassiveGasSpent < passiveGasSpendingLimit);
+
         uint beginGas = msg.gas; //Record remaining at start
-        
+
         _; //Execute function
-        
+
         uint endGas = msg.gas; //Record remaining at end
         uint totalGas = beginGas - endGas; //msg.gas is gas remaining, so begin > end
         uint totalPrice = totalGas * tx.gasprice; //The total amount of gas
-        
-        emit PassiveGasSpent(gasAmount);
-        
-        msg.sender.send(totalGas, totalPassiveGasSpent, passiveGasSpendingLimit);
-        totalSpent += totalPrice;
+
+        //TODO Give back gas to owner
+        //msg.gas = msg.gas + totalGas;
+
+        require(totalGas + totalPassiveGasSpent < passiveGasSpendingLimit);
+
+        PassiveGasSpent(totalGas, totalPassiveGasSpent, passiveGasSpendingLimit);
+
+        msg.sender.send(totalGas);
+        totalPassiveGasSpent += totalGas;
     }
-    
+
+    /**
+     * @dev Set the spending limit for passive gas.
+     */
     function setPassiveGasSpendingLimit(uint limit) onlyOwner public {
-        require(msg.value == limit);
         passiveGasSpendingLimit = limit;
     }
-    
+
+    /**
+     * @dev Add passive gas to this contract.
+     */
     function addSpending() onlyOwner public {
-        require(msg.value == limit);
-        totalSpent = 0;
+        require(msg.value > 0);
+        totalPassiveGasSpent = 0;
     }
-    
+
     function getTotalPassiveGasSpent() onlyOwner view returns (uint) {
         return totalPassiveGasSpent;
     }
-    
+
     function getPassiveGasSpendingLimit() onlyOwner view returns (uint) {
         return passiveGasSpendingLimit;
     }
-    
+
     function pause() onlyOwner public {
         paused = true;
     }
-    
+
     function resume() onlyOwner public {
         paused = false;
     }
